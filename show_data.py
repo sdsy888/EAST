554612bb7b44dac2a59aefc0da17854ad39d1e41
sdsy888 <sdsy888@users.noreply.github.com>
2017-11-01 16:14:10 +0800

    update .gitignore

--- show_data.py
+++ show_data.py
@@ -14,16 +14,17 @@ plt.rcParams['image.interpolation'] = 'nearest'
 plt.rcParams['image.cmap'] = 'gray'
 
 # name = input("What is your name? ")
-dataPath = "/home/neo/Dataset/ICDAR2015/TextLocalization/TextLocalization_test_images"
 
-DATA_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'train')
+DATA_PATH = "/home/neo/Dataset/ICDAR2015/TextLocalization/TextLocalization_test_images"
 
-image_list = glob.glob(os.path.join(DATA_PATH, '*', 'image*.jpg'))
+image_list = glob.glob(os.path.join(DATA_PATH, '*.jpg'))
+labelPath = "/home/neo/TFtoys/EAST/result"
 
 
-def show_all():
+# 标签文件既有坐标也有内容且一起显示
+def show_all_with_label():
     for image in image_list:
-        label_file_name = image[:-4] + '.txt'
+        label_file_name = os.path.join(labelPath, "res_" + image.split('/')[-1][:-4] + '.txt')
         im = Image.open(image)
         im = np.array(im).astype(np.uint8)
         plt.imshow(im)
@@ -40,12 +41,36 @@ def show_all():
             # ,bbox={'facecolor':'white', 'alpha':0.5}
             plt.text(location[0], location[1], context, fontsize=8, color='#33FF36')
         plt.show()
-        name = input("Are you continue? ")
+        name = input("Continue? ")
         if name == 'n' or name == 'no':
             break
 
 
-def show_num(num):
+# 标签文件只有坐标，没有内容
+def show_all_box():
+    for image in image_list:
+        label_file_name = os.path.join(labelPath, "res_" + image.split('/')[-1][:-4] + '.txt')
+        im = Image.open(image)
+        im = np.array(im).astype(np.uint8)
+        plt.imshow(im)
+        boxes = codecs.open(label_file_name, 'r', 'utf8').readlines()
+
+        for box in boxes:
+            box = box.strip().split(',')
+            location = [int(i) for i in box]
+            # print(location)
+            plt.plot([location[0], location[2], location[4], location[6], location[0]], [
+                     location[1], location[3], location[5], location[7], location[1]], 'r-')
+            # ,bbox={'facecolor':'white', 'alpha':0.5}
+            # plt.text(location[0], location[1], context, fontsize=8, color='#33FF36')
+        plt.show()
+        name = raw_input("Continue?")
+        if name == 'n' or name == 'no':
+            break
+
+
+# 显示指定标号的box和内容标签
+def show_num_with_box(num):
     img_name = 'image_' + str(num) + '.jpg'
     for image in image_list:
         if img_name in image:
@@ -70,6 +95,7 @@ def show_num(num):
 
 
 if __name__ == '__main__':
-    n = int(input("num"))
-    show_num(n)
+    # n = int(input("num"))
+    # show_num(n)
+    show_all_box()
 # print(image_list[0:2],'\n',label_file_name)
